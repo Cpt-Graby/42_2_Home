63989e5f97a4f0d75bfbaec7e2ae7d5a3056826a
Cpt-Graby <alexisgonelle.mobile@gmail.com>
2022-11-06 12:30:57 +0100

    Correction de synthax

--- 42_2_home.sh
+++ 42_2_home.sh
@@ -4,15 +4,17 @@ set -o errexit
 #Voici un petit script pour te simplifier la vie pour tes transferts entre la 
 #Vogosphere et ton github.
 
-Path_git="..."
-Path_vogo="..."
+Path_git="/home/kino/Desktop/test1"
+Path_vogo="/home/kino/Desktop/test2"
+#Path_git="..."
+#Path_vogo="..."
 update_message="Mise a jour de mon git. "
 version=0.1
 
 transf_2_git (){
-	(cd Path_vogo; git add -A; git commit -m "$update_message "; git push)
-	cp -R -v $Path_vogo $Path_git 
-	(cd $Path_git; git add -A; git commit -m "$update_message  "; git push)
+	(cd ${Path_vogo}; git add -A; git commit -m "${update_message} "; git push)
+	cp -R -v ${Path_vogoi} ${Path_git}
+	(cd ${Path_git}; git add -A; git commit -m "${update_message} "; git push)
 }
 
 help_message (){
@@ -22,7 +24,7 @@ help_message (){
 	echo "	Start you day and copy everything from your git to"
 	echo "	the Vogosphere dir."
 	echo "-F"
-	echo "	End your day by pushing all in Vogosphere and then"
+	echo "	You finish your day pushing all in Vogosphere and by"
 	echo "	copying everything in your Git dir to finaly push it."
 	echo "	You can use this argument with a second one, to state"
 	echo "	your git commit -m."
@@ -32,22 +34,23 @@ help_message (){
 
 print_path (){
 	echo "You are working with those paths:"
-	echo "Path git:		$Path_git"
-	echo "Path vogosphere:	$Path_vogo"
+	echo "Path git:		${Path_git}"
+	echo "Path vogosphere:	${Path_vogo}"
 }
 
-if [Path_git = "..." || Path_vogo = "..."]
+if [[ "${Path_git}" == "..." || "${Path_vogo}" == "..." ]]; then
 	echo "Please update the working paths!"
 	exit 0
 fi
-if [$1 = "-S"]; then
-	cp -R $Path_git $Path_vogo
-	echo "All files were transfered" 
-elif [$1 = "-P"]; then
+echo "Hello Sir"
+if [ "$1" == "-S" ]; then
+	cp -R -v ${Path_git} ${Path_vogo}
+	echo "All files were transfered!" 
+elif [ "$1" == "-P" ]; then
 	print_path()
-elif [$1 = "-F"]; then
-	echo "You probably worked hard today;"
-	if [ $# eq 2]; then
+elif [ "$1" == "-F" ]; then
+	echo "You probably worked hard today."
+	if [ $# eq 2 ]; then
 		update_message=$2
 	fi
 	transf_2_git()
