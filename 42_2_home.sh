90b91fef43af603141de00b15ff3178004771023
Cpt-Graby <alexisgonelle.mobile@gmail.com>
2022-11-06 07:13:19 +0100

    First commit of a little script

---
+++ 42_2_home.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -o errexit
+
+#Voici un petit script pour te simplifier la vie pour tes transferts entre la 
+#Vogosphere et ton github.
+
+Path_git="..."
+Path_vogo="..."
+update_message="Mise a jour de mon git. "
+version=0.1
+
+transf_2_git (){
+	(cd Path_vogo; git add -A; git commit -m "$update_message "; git push)
+	cp -R -v $Path_vogo $Path_git 
+	(cd $Path_git; git add -A; git commit -m "$update_message  "; git push)
+}
+
+help_message (){
+	echo "Thx for using 42_2_Home script."
+	echo "For now there is only 3 arguments for this functions"
+	echo "-S "
+	echo "	Start you day and copy everything from your git to"
+	echo "	the Vogosphere dir."
+	echo "-F"
+	echo "	End your day by pushing all in Vogosphere and then"
+	echo "	copying everything in your Git dir to finaly push it."
+	echo "	You can use this argument with a second one, to state"
+	echo "	your git commit -m."
+	echo "-P"
+	echo "	Print the path that the script is using"
+}
+
+print_path (){
+	echo "You are working with those paths:"
+	echo "Path git:		$Path_git"
+	echo "Path vogosphere:	$Path_vogo"
+}
+
+if [Path_git = "..." || Path_vogo = "..."]
+	echo "Please update the working paths!"
+	exit 0
+fi
+if [$1 = "-S"]; then
+	cp -R $Path_git $Path_vogo
+	echo "All files were transfered" 
+elif [$1 = "-P"]; then
+	print_path()
+elif [$1 = "-F"]; then
+	echo "You probably worked hard today;"
+	if [ $# eq 2]; then
+		update_message=$2
+	fi
+	transf_2_git()
+	echo "Everything has been copied && and uploaded"
+else
+	help_message()
+fi
+exit 0
