f0051cf53e00d13c99a7d7745f6504cd40d09cfb
Cpt-Graby <alexisgonelle.mobile@gmail.com>
2022-11-06 16:55:07 +0100

    Correction du shell

--- 42_2_home.sh
+++ 42_2_home.sh
@@ -4,21 +4,19 @@ set -o errexit
 #Voici un petit script pour te simplifier la vie pour tes transferts entre la 
 #Vogosphere et ton github.
 
-Path_git="/home/kino/Desktop/test1"
-Path_vogo="/home/kino/Desktop/test2"
-#Path_git="..."
-#Path_vogo="..."
+Path_git="..."
+Path_vogo="..."
 update_message="Mise a jour de mon git. "
 version=0.1
 
-transf_2_git (){
-	(cd ${Path_vogo}; git add -A; git commit -m "${update_message} "; git push)
-	cp -R -v ${Path_vogoi} ${Path_git}
-	(cd ${Path_git}; git add -A; git commit -m "${update_message} "; git push)
+function transf_2_git (){
+	(cd ${Path_vogo}; git add -A; git commit -m "$1 "; git push)
+	cp -R -v ${Path_vogo}/* ${Path_git}
+	echo "commit -m :$update_message "
+	(cd ${Path_git}; git add -A; git commit -m "$1 "; git push)
 }
 
-help_message (){
-	echo "Thx for using 42_2_Home script."
+function help_message (){
 	echo "For now there is only 3 arguments for this functions"
 	echo "-S "
 	echo "	Start you day and copy everything from your git to"
@@ -32,30 +30,32 @@ help_message (){
 	echo "	Print the path that the script is using"
 }
 
-print_path (){
+function print_path (){
 	echo "You are working with those paths:"
 	echo "Path git:		${Path_git}"
 	echo "Path vogosphere:	${Path_vogo}"
 }
 
-if [[ "${Path_git}" == "..." || "${Path_vogo}" == "..." ]]; then
+if [[ "${Path_git}" = "..." || "${Path_vogo}" == "..." ]]; then
+	print_path
 	echo "Please update the working paths!"
 	exit 0
 fi
-echo "Hello Sir"
-if [ "$1" == "-S" ]; then
-	cp -R -v ${Path_git} ${Path_vogo}
+echo "Hello there, thx for using the 42_2_Home script."
+if [ "$1" = "-S" ]; then
+	cp -R -v ${Path_git}/* ${Path_vogo}
 	echo "All files were transfered!" 
-elif [ "$1" == "-P" ]; then
-	print_path()
-elif [ "$1" == "-F" ]; then
+
+elif [ "$1" = "-P" ]; then
+	print_path
+elif [ "$1" = "-F" ]; then
 	echo "You probably worked hard today."
-	if [ $# eq 2 ]; then
+	if [ $# = 2 ]; then
 		update_message=$2
 	fi
-	transf_2_git()
+	transf_2_git 
 	echo "Everything has been copied && and uploaded"
 else
-	help_message()
+	help_message
 fi
 exit 0
