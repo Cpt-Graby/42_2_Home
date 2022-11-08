69941220f6e9e14342cca2c9565621b4f21d645e
Cpt-Graby <alexisgonelle.mobile@gmail.com>
2022-11-08 11:16:06 +0100

    version 0.2

--- 42_2_home.sh
+++ 42_2_home.sh
@@ -7,12 +7,13 @@ set -o errexit
 Path_git="..."
 Path_vogo="..."
 update_message="Mise a jour de mon git. "
-version=0.1
+version=0.2
 
 function transf_2_git (){
+	echo "Lets push to your Vogosphere (${Path_vogo})."
 	(cd ${Path_vogo}; git add -A; git commit -m "${update_message} "; git push)
 	cp -R -v ${Path_vogo}/* ${Path_git}
-	echo "commit -m :${update_message}"
+	echo "All is copied. Lets push your git."
 	(cd ${Path_git}; git add -A; git commit -m "${update_message} "; git push)
 }
 
@@ -36,12 +37,20 @@ function print_path (){
 	echo "Path vogosphere:	${Path_vogo}"
 }
 
+function name_prog(){
+	echo "  __  ___        ___        _   _  _____  __  __  ____   "
+	echo " /. |(__ \      (__ \      ( )_( )(  _  )(  \/  )( ___)  "
+	echo "(_  _)/ _/  ___  / _/  ___  ) _ (  )(_)(  )    (  )__)  "
+	echo " (_)(____)(___)(____)(___)(_) (_)(_____)(_/\/\_)(____)  "
+	echo " version: ${version}"
+}
+
 if [[ "${Path_git}" = "..." || "${Path_vogo}" == "..." ]]; then
 	print_path
 	echo "Please update the working paths!"
 	exit 0
 fi
-echo "Hello there, thx for using the 42_2_Home script."
+name_prog
 if [ "$1" = "-S" ]; then
 	cd ${Path_git}
 	git pull
